c5f4f039e0e4792f9fed1dab3464d9181b8821f6
KVZLPT70 <KVZLPT70@KVZ-LPT-70>
2023-06-15 10:25:32 +0530

    second line

--- src/com/test/JavaAppTest.java
+++ src/com/test/JavaAppTest.java
@@ -5,6 +5,11 @@ public class JavaAppTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("My first application");
+		System.out.println("hi");
+		System.out.println("hi");
+		System.out.println("hi");
+		System.out.println("hi");
+		
 	}
 
 }
